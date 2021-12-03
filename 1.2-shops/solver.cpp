1d90027a2f64f79c0f99a3fd4145807b8805c838
Marios Kerasiotis <39958515+mariosker@users.noreply.github.com>
2021-12-03 19:43:45 +0200

    two bad cases

--- 1.2-shops/solver.cpp
+++ 1.2-shops/solver.cpp
@@ -41,26 +41,42 @@ class City {
  public:
   int buildings;
   int buyer_count;
-  FenwickTree* tenants;
+  vector<int> tenants;
+  FenwickTree* sum_tenants;
   vector<int> K;
 
-  void set(int buildings_, int buyer_count_) {
-    buildings = buildings_;
-    buyer_count = buyer_count_;
-    tenants = new FenwickTree(buildings_);
+  bool get_input() {
+    int tenant_count;
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> buildings >> buyer_count;
+
     K.resize(buyer_count + 1, INT_MAX);
+    tenants.resize(buildings, 0);
+    sum_tenants = new FenwickTree(buildings);
+
+    for (int i = 0; i < buildings; i++) {
+      cin >> tenant_count;
+      if (tenant_count == buyer_count) return true;
+      tenants[i] = tenant_count;
+      sum_tenants->add(i, tenant_count);
+    }
+
+    return false;
   }
 
   int check_sum(int y, long int target) {
-    long int current_sum;
+    // long int current_sum = 0;
 
-    for (int j = y; j < buildings; j++) {
-      current_sum = tenants->sum(y, j);
-      if (current_sum <= buyer_count)
-        K[current_sum] = min(K[current_sum], j - y + 1);
-      else
-        break;
-    }
+    // for (int j = y; j < buildings; j++) {
+    //   current_sum += tenants[j];
+    //   if (current_sum <= buyer_count)
+    //     K[current_sum] = min(K[current_sum], j - y + 1);
+    //   else
+    //     break;
+    // }
 
     return (K[target] != INT_MAX) ? K[target] : -1;
   }
@@ -68,20 +84,24 @@ class City {
   int find_min() {
     int min_length = INT_MAX;
     int right_length = 0;
-    long int current_sum = 0;
+    long int current_sum;
+    long int total_sum = sum_tenants->sum(0, buildings - 1);
 
     for (int y = buildings - 1; y >= 0; y--) {
+      current_sum = total_sum;
       for (int x = 0; x <= y; x++) {
-        current_sum = tenants->sum(x, y);
         if (current_sum <= buyer_count) K[current_sum] = min(K[current_sum], y - x + 1);
 
         if (current_sum == buyer_count)
           min_length = min(min_length, y - x + 1);
+
         else if (current_sum < buyer_count) {
           right_length = check_sum(y + 1, buyer_count - current_sum);
           if (right_length != -1) min_length = min(min_length, right_length + y - x + 1);
         }
+        current_sum -= tenants[x];
       }
+      total_sum -= tenants[y];
     }
 
     return (min_length != INT_MAX) ? min_length : -1;
@@ -89,31 +109,13 @@ class City {
 
   City(){};
 
-  ~City() { delete tenants; };
+  ~City() { delete sum_tenants; };
 };
-
-bool get_input(City& city) {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-
-  int buildings;
-  int buyer_count;
-  int tenant_count;
-  cin >> buildings >> buyer_count;
-  city.set(buildings, buyer_count);
-
-  for (int i = 0; i < buildings; i++) {
-    cin >> tenant_count;
-    if (tenant_count == buyer_count) return true;
-    city.tenants->add(i, tenant_count);
-  }
-  return false;
-}
 
 int main() {
   City city;
   int distance;
-  if (get_input(city))
+  if (city.get_input())
     distance = 1;
   else {
     distance = city.find_min();
