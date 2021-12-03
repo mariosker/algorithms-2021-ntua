e477deade9ee849e3a6a6bead13040b4e8e14424
Marios Kerasiotis <39958515+mariosker@users.noreply.github.com>
2021-12-03 18:59:04 +0200

    working but a little slow

--- 1.2-shops/solver.cpp
+++ 1.2-shops/solver.cpp
@@ -11,39 +11,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Sum_details {
-  int s;
-  int f;
-  int dif;
-
-  Sum_details(int s, int f) {
-    this->s = s;
-    this->f = f;
-    this->dif = f - s + 1;
-  }
-};
 struct FenwickTree {
-  vector<int> bit;  // binary indexed tree
-  int n;
+  vector<long int> bit;  // binary indexed tree
+  long int n;
 
-  FenwickTree(int n) {
+  FenwickTree(long int n) {
     this->n = n;
     bit.assign(n, 0);
   }
 
-  FenwickTree(vector<int> a) : FenwickTree(a.size()) {
+  FenwickTree(vector<long int> a) : FenwickTree(a.size()) {
     for (size_t i = 0; i < a.size(); i++) add(i, a[i]);
   }
 
-  int sum(int r) {
-    int ret = 0;
+  long int sum(long int r) {
+    long int ret = 0;
     for (; r >= 0; r = (r & (r + 1)) - 1) ret += bit[r];
     return ret;
   }
 
-  int sum(int l, int r) { return sum(r) - sum(l - 1); }
+  long int sum(long int l, long int r) { return sum(r) - sum(l - 1); }
 
-  void add(int idx, int delta) {
+  void add(long int idx, long int delta) {
     for (; idx < n; idx = idx | (idx + 1)) bit[idx] += delta;
   }
 };
@@ -53,73 +42,48 @@ class City {
   int buildings;
   int buyer_count;
   FenwickTree* tenants;
-  vector<vector<Sum_details>> k_sums;
+  vector<int> K;
 
   void set(int buildings_, int buyer_count_) {
     buildings = buildings_;
     buyer_count = buyer_count_;
     tenants = new FenwickTree(buildings_);
+    K.resize(buyer_count + 1, INT_MAX);
   }
 
-  void fill_K_sums() {
-    int s;
-    k_sums.resize(buyer_count + 1);
-    for (int i = 0; i < buildings; i++) {
-      for (int j = i; j < buildings; j++) {
-        s = tenants->sum(i, j);
-        if (s <= buyer_count) {
-          k_sums[s].push_back(Sum_details(i, j));
-        }
-      }
+  int check_sum(int y, long int target) {
+    long int current_sum;
+
+    for (int j = y; j < buildings; j++) {
+      current_sum = tenants->sum(y, j);
+      if (current_sum <= buyer_count)
+        K[current_sum] = min(K[current_sum], j - y + 1);
+      else
+        break;
     }
 
-    for (int i = 0; i < buyer_count; i++) {
-      sort(k_sums[i].begin(), k_sums[i].end(),
-           [](Sum_details const& stud1, Sum_details const& stud2) -> bool {
-             return stud1.dif < stud2.dif;
-           });
-    }
-  }
-
-  bool are_nonoverlapping(const Sum_details& a, const Sum_details& b) {
-    return (a.f < b.s || b.f < a.s);
+    return (K[target] != INT_MAX) ? K[target] : -1;
   }
 
   int find_min() {
     int min_length = INT_MAX;
-    int s1;
-    int s2;
-    int index1;
-    int index2;
-    for (int i = 1; i < buyer_count / 2; i++) {
-      int s1 = k_sums[i].size();
-      int s2 = k_sums[buyer_count - i].size();
-      if (s1 && s2) {
-        index1 = 0;
-        index2 = 0;
+    int right_length = 0;
+    long int current_sum = 0;
 
-        while (index1 < s1 - 1 || index2 < s2 - 1) {
-          if (are_nonoverlapping(k_sums[i][index1], k_sums[buyer_count - i][index2])) {
-            min_length =
-                min(min_length, k_sums[i][index1].dif + k_sums[buyer_count - i][index2].dif);
-            break;
-          } else {
-            if (k_sums[i][index1].dif < k_sums[buyer_count - i][index2].dif) {
-              if (index2 < s2 - 1)
-                index2++;
-              else if (index1 < s1 - 1)
-                index1++;
-            } else if (k_sums[i][index1].dif >= k_sums[buyer_count - i][index2].dif) {
-              if (index1 < s1 - 1)
-                index1++;
-              else if (index2 < s2 - 1)
-                index2++;
-            }
-          }
+    for (int y = buildings - 1; y >= 0; y--) {
+      for (int x = 0; x <= y; x++) {
+        current_sum = tenants->sum(x, y);
+        if (current_sum <= buyer_count) K[current_sum] = min(K[current_sum], y - x + 1);
+
+        if (current_sum == buyer_count)
+          min_length = min(min_length, y - x + 1);
+        else if (current_sum < buyer_count) {
+          right_length = check_sum(y + 1, buyer_count - current_sum);
+          if (right_length != -1) min_length = min(min_length, right_length + y - x + 1);
         }
       }
     }
-    if (k_sums[buyer_count].size()) min_length = min(min_length, k_sums[buyer_count][0].dif);
+
     return (min_length != INT_MAX) ? min_length : -1;
   }
 
@@ -152,7 +116,6 @@ int main() {
   if (get_input(city))
     distance = 1;
   else {
-    city.fill_K_sums();
     distance = city.find_min();
   }
   cout << distance << endl;
