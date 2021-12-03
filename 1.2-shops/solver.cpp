122d48a404196f198c40d2352011858f9fef1bb6
Marios Kerasiotis <39958515+mariosker@users.noreply.github.com>
2021-12-03 11:10:14 +0200

    throws seg fault

--- 1.2-shops/solver.cpp
+++ 1.2-shops/solver.cpp
@@ -11,6 +11,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct Sum_details {
+  int s;
+  int f;
+  int dif;
+
+  Sum_details(int s, int f) {
+    this->s = s;
+    this->f = f;
+    this->dif = f - s + 1;
+  }
+};
 struct FenwickTree {
   vector<int> bit;  // binary indexed tree
   int n;
@@ -41,20 +52,83 @@ class City {
  public:
   int buildings;
   int buyer_count;
-  FenwickTree *tenants;
-  vector<vector<int>> caching;
+  FenwickTree* tenants;
+  vector<vector<Sum_details>> k_sums;
 
   void set(int buildings_, int buyer_count_) {
     buildings = buildings_;
     buyer_count = buyer_count_;
     tenants = new FenwickTree(buildings_);
-    caching.resize(buildings, vector<int>(buildings, -1));
   }
+
+  void fill_K_sums() {
+    int s;
+    k_sums.resize(buyer_count + 1);
+    for (int i = 0; i < buildings; i++) {
+      for (int j = i; j < buildings; j++) {
+        s = tenants->sum(i, j);
+        if (s <= buyer_count) {
+          k_sums[s].push_back(Sum_details(i, j));
+        }
+      }
+    }
+
+    for (int i = 0; i < buyer_count; i++) {
+      sort(k_sums[i].begin(), k_sums[i].end(),
+           [](Sum_details const& stud1, Sum_details const& stud2) -> bool {
+             return stud1.dif < stud2.dif;
+           });
+    }
+  }
+
+  bool are_nonoverlapping(const Sum_details& a, const Sum_details& b) {
+    return (a.f < b.s || b.f < a.s);
+  }
+
+  int find_min() {
+    int min_length = INT_MAX;
+    int s1;
+    int s2;
+    int index1;
+    int index2;
+    for (int i = 1; i < buyer_count / 2; i++) {
+      int s1 = k_sums[i].size();
+      int s2 = k_sums[buyer_count - i].size();
+      if (s1 && s2) {
+        index1 = 0;
+        index2 = 0;
+
+        while (index1 < s1 - 1 || index2 < s2 - 1) {
+          if (are_nonoverlapping(k_sums[i][index1], k_sums[buyer_count - i][index2])) {
+            min_length =
+                min(min_length, k_sums[i][index1].dif + k_sums[buyer_count - i][index2].dif);
+            break;
+          } else {
+            if (k_sums[i][index1].dif < k_sums[buyer_count - i][index2].dif) {
+              if (index2 < s2 - 1)
+                index2++;
+              else if (index1 < s1 - 1)
+                index1++;
+            } else if (k_sums[i][index1].dif >= k_sums[buyer_count - i][index2].dif) {
+              if (index1 < s1 - 1)
+                index1++;
+              else if (index2 < s2 - 1)
+                index2++;
+            }
+          }
+        }
+      }
+    }
+    if (k_sums[buyer_count].size()) min_length = min(min_length, k_sums[buyer_count][0].dif);
+    return (min_length != INT_MAX) ? min_length : -1;
+  }
+
   City(){};
+
   ~City() { delete tenants; };
 };
 
-bool get_input(City &city) {
+bool get_input(City& city) {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
 
@@ -72,62 +146,15 @@ bool get_input(City &city) {
   return false;
 }
 
-int min_length_subarray_with_target_sum(int idx_s, City &city, int target_sum) {
-  int min_length = INT_MAX;
-  int subaray_sum = 0;
-
-  for (int i = idx_s; i < city.buildings; i++) {
-    for (int j = i; j < city.buildings; j++) {
-      if (city.caching[i][j] == -1) {
-        subaray_sum = city.tenants->sum(i, j);
-        city.caching[i][j] = subaray_sum;
-      } else {
-        subaray_sum = city.caching[i][j];
-      }
-
-      if (subaray_sum > target_sum) {
-        break;
-      } else if (subaray_sum == target_sum) {
-        min_length = min(min_length, j - i + 1);
-      }
-    }
-  }
-  return min_length;
-}
-
-int min_length_subarrays(City &city) {
-  int min_length = INT_MAX;
-
-  int subaray_sum = 0;
-
-  for (int i = 0; i < city.buildings; i++) {
-    for (int j = i; j < city.buildings; j++) {
-      if (city.caching[i][j] == -1) {
-        subaray_sum = city.tenants->sum(i, j);
-        city.caching[i][j] = subaray_sum;
-      } else {
-        subaray_sum = city.caching[i][j];
-      }
-      if (subaray_sum > city.buyer_count) {
-        break;
-      } else if (subaray_sum == city.buyer_count) {
-        min_length = min(min_length, j - i + 1);
-      } else {
-        int res = min_length_subarray_with_target_sum(
-            j + 1, city, city.buyer_count - subaray_sum);
-        if (res != INT_MAX) min_length = min(min_length, j - i + 1 + res);
-      }
-    }
-  }
-  return min_length;
-}
-
 int main() {
   City city;
   int distance;
   if (get_input(city))
     distance = 1;
-  else
-    distance = min_length_subarrays(city);
-  cout << (distance != INT_MAX ? distance : -1) << endl;
+  else {
+    city.fill_K_sums();
+    distance = city.find_min();
+  }
+  cout << distance << endl;
+  return 0;
 }
